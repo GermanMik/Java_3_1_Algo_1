060626d51131b4091db55baadd6f491c83d06903
GermanMik <tiktak@yandex.ru>
2018-10-01 23:29:00 +0300

    trying to change a count style

--- src/ru/geekbrains/java3_1/algo1/Main.java
+++ src/ru/geekbrains/java3_1/algo1/Main.java
@@ -12,18 +12,40 @@ public class Main {
         System.out.println("Введите степень");
         String intN = input.next();
         int n = Integer.valueOf(intN);
-
-        //Возведение в степень
-        int c = vozvedeineVStepen( a, n);
-        System.out.println("Результат возведения в степень: "+c);
+        int finalresult = vozvedeineVStepenChetnoe(a, n);
+        System.out.println("Результат возведения в степень: " + finalresult);
     }
 
-    //Возведение в степень
-    private static int vozvedeineVStepen(int a, int n) {
+//    //Возведение в степень
+//    public static int finalResult(int a, int n) {
+//        int xresult = 1;
+//        if (n % 2 == 1) {
+//            vozvedeineVStepenChetnoe(a, n);
+//        } else {
+//            vozvedeineVStepen(a, n);
+//        }
+//        return;
+//
+//    }
+//
+    //Возведение в степень четное
+    private static int vozvedeineVStepenChetnoe(int a, int n) {
+        int result = 1;
+        for (int i = 1; i <= (n - 1); i++) {
+            result = result * a;
+        }
+        return result * a;
 
-        if (n == 0) return 1; // на ноль умножать нельзя
-        return vozvedeineVStepen(a, n - 1) * a;
+
     }
+//
+//    //Возведение в степень
+//    private static int vozvedeineVStepen(int a, int n) {
+//        int result = 1;
+//        for (int i = 1; i <= (n); i++) {
+//            result = result * a;
+//        }
+//        return result;
+//    }
 }
-
 
